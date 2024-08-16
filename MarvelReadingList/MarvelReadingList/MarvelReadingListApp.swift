1f90213d224003f8d2fe28e748a3a46487503347
Alfredo Colon <alfcolon@gmail.com>
2024-08-16 13:14:07 -0400

    Added xcode project

---
+++ MarvelReadingList/MarvelReadingList/MarvelReadingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelReadingListApp.swift
+//  MarvelReadingList
+//
+//  Created by Alfredo Colon on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelReadingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
